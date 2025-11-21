c29a501a0097ccafc7e8dd7691c68863f07cd7dc
JayClock <35857909+JayClock@users.noreply.github.com>
2025-11-21 22:33:12 +0800

    feat(state): add conditional typing for entity relations
    Update ResourceState to only expose relation methods (links, follow,
    getForm) when the entity has relations defined.

--- packages/resource/src/lib/state/interface.ts
+++ packages/resource/src/lib/state/interface.ts
@@ -40,14 +40,17 @@ export type State<TEntity extends Entity = Entity> = {
 
 export type ResourceState<TEntity extends Entity = Entity> = Omit<
   State<TEntity>,
-  'data' | 'collection'
+  'data' | 'collection' | 'links' | 'follow' | 'getForm'
 > &
   (keyof TEntity['description'] extends never
     ? unknown
     : { data: TEntity['description'] }) &
   (IsCollectionType<TEntity> extends true
     ? { collection: StateCollection<TEntity> }
-    : unknown);
+    : unknown) &
+  (keyof TEntity['relations'] extends never
+    ? unknown
+    : Pick<State<TEntity>, 'links' | 'follow' | 'getForm'>);
 
 export type Form = {
   /**
