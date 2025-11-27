022cc08aa9291e75880ae38e928ab233f5c6d4b2
JayClock <35857909+JayClock@users.noreply.github.com>
2025-11-27 23:32:04 +0800

    deps(resource): wire up client dependencies with IoC container
    Sets up Inversify container bindings for configuration, client, and fetcher
    to enable dependency injection for resource client instantiation.

---
+++ packages/resource/src/lib/create-client.ts
@@ -0,0 +1,13 @@
+import { Client } from './client.js';
+import { Container } from 'inversify';
+import { TYPES } from './archtype/injection-types.js';
+import { Config } from './archtype/config.js';
+import { Fetcher } from './http/fetcher.js';
+
+export const createClient = (options: Config) => {
+  const container = new Container();
+  container.bind(TYPES.Config).toConstantValue(options);
+  container.bind(TYPES.Client).to(Client).inSingletonScope();
+  container.bind(TYPES.Fetcher).to(Fetcher).inSingletonScope();
+  return container.get(Client);
+};
