d2a1560d02fe868b15115ba48e6f77202c18108e
JayClock <35857909+JayClock@users.noreply.github.com>
2025-12-11 09:33:31 +0800

    feat(resource): enhance Links class with additional query methods
    Add new methods to Links class for improved link management:
    - set() method to replace existing links
    - getMany() to retrieve multiple links by relation
    - getAll() to fetch all stored links
    - has() to check link existence
    
    Update test suite with comprehensive coverage for new functionality
    including edge cases and multiple link scenarios.

--- packages/resource/src/lib/links/links.ts
+++ packages/resource/src/lib/links/links.ts
@@ -10,16 +10,57 @@ export class Links<T extends Record<string, SafeAny>> {
         // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
         this.store.get(link.rel)!.push(link);
       } else {
-        this.store.set(link.rel, links);
+        this.set(link);
       }
     }
   }
 
+  /**
+   * Return a single link by its 'rel'.
+   *
+   * If the link does not exist, undefined is returned.
+   */
   get(rel: keyof T): Link | undefined {
     if (this.store.has(rel as string)) {
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       return this.store.get(rel as string)![0];
     }
     return undefined;
+  }
+
+  /**
+   * Set a link
+   *
+   * If a link with the provided 'rel' already existed, it will be overwritten.
+   */
+  set(link: Link): void {
+    this.store.set(link.rel, [link]);
+  }
+
+  /**
+   * Return all links that have a given rel.
+   *
+   * If no links with the rel were found, an empty array is returned.
+   */
+  getMany(rel: keyof T): Link[] {
+    return this.store.get(rel as string) || [];
+  }
+
+  /**
+   * Return all links.
+   */
+  getAll(): Link[] {
+    const result = [];
+    for (const links of this.store.values()) {
+      result.push(...links);
+    }
+    return result;
+  }
+
+  /**
+   * Returns true if at least 1 link with the given rel exists.
+   */
+  has(rel: keyof T): boolean {
+    return this.store.has(rel as string);
   }
 }
--- packages/resource/src/tests/links.spec.ts
+++ packages/resource/src/tests/links.spec.ts
@@ -3,11 +3,49 @@ import { Links } from '../lib/links/links.js';
 import { Link } from '../lib/links/link.js';
 
 describe('Links', () => {
-  const links = new Links<Record<string, Link>>();
+  let links: Links<Record<string, Link>>;
+  beforeEach(() => {
+    links = new Links<Record<string, Link>>();
+  });
 
-  it('should get single stored link', () => {
-    const link: Link = { rel: 'rel', href: 'href' };
+  it('should add multi links', () => {
+    const link1: Link = { rel: 'rel1', href: 'href1' };
+    const link2: Link = { rel: 'rel2', href: 'href2' };
+    links.add([link1, link2]);
+    expect(links.get('rel1')).toEqual(link1);
+    expect(links.get('rel2')).toEqual(link2);
+  });
+
+  it('should set single link and instead exist link', () => {
+    const link1: Link = { rel: 'rel', href: 'href1' };
+    const link2: Link = { rel: 'rel', href: 'href2' };
+    links.add([link1]);
+    links.set(link2);
+    expect(links.get('rel')).toEqual(link2);
+  });
+
+  it('should get multiple links', () => {
+    const link1: Link = { rel: 'rel', href: 'href1' };
+    const link2: Link = { rel: 'rel', href: 'href2' };
+    const link3: Link = { rel: 'rel3', href: 'href3' };
+    links.add([link1, link2, link3]);
+    expect(links.getMany('rel')).toEqual([link1, link2]);
+    expect(links.getMany('rel3')).toEqual([link3]);
+    expect(links.getMany('not existed')).toEqual([]);
+  });
+
+  it('should get all links', () => {
+    const link1: Link = { rel: 'rel', href: 'href1' };
+    const link2: Link = { rel: 'rel', href: 'href2' };
+    const link3: Link = { rel: 'rel3', href: 'href3' };
+    links.add([link1, link2, link3]);
+    expect(links.getAll()).toEqual([link1, link2, link3]);
+  });
+
+  it('It should be determined whether a link exists', () => {
+    const link: Link = { rel: 'rel', href: 'href1' };
     links.add([link]);
-    expect(links.get('rel')).toEqual(link);
+    expect(links.has('rel')).toBeTruthy();
+    expect(links.has('not existed')).toBeFalsy();
   });
 });
