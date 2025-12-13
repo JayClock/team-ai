4fdb3e6cbab5de961591b4ebbb7de50501b9d5dd
JayClock <35857909+JayClock@users.noreply.github.com>
2025-12-13 21:28:24 +0800

    feat: add resource-react

---
+++ packages/resource-react/README.md
@@ -0,0 +1,7 @@
+# @hateoas/resource-react
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test @hateoas/resource-react` to execute the unit tests via [Vitest](https://vitest.dev/).
---
+++ packages/resource-react/src/lib/resource-react.tsx
@@ -0,0 +1,11 @@
+import styles from './resource-react.module.css';
+
+export function HateoasResourceReact() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to HateoasResourceReact!</h1>
+    </div>
+  );
+}
+
+export default HateoasResourceReact;
