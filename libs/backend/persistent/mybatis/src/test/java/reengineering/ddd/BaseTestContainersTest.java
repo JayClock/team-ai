cbef25c79a3bc551ed3ed2fa0326ca8370ff4089
JayClock <35857909+JayClock@users.noreply.github.com>
2026-01-06 23:34:04 +0800

    🐛 revert: restore TestContainers in CI and remove unused project config

--- libs/backend/persistent/mybatis/src/test/java/reengineering/ddd/BaseTestContainersTest.java
+++ libs/backend/persistent/mybatis/src/test/java/reengineering/ddd/BaseTestContainersTest.java
@@ -7,37 +7,20 @@ import org.testcontainers.utility.DockerImageName;
 
 public abstract class BaseTestContainersTest {
 
-  private static final boolean CI_ENVIRONMENT = "true".equals(System.getenv("CI"));
-  private static final PostgreSQLContainer<?> postgres;
+  private static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(DockerImageName.parse("postgres:15-alpine"))
+    .withDatabaseName("testdb")
+    .withUsername("test")
+    .withPassword("test")
+    .withReuse(true);
 
   static {
-    if (CI_ENVIRONMENT) {
-      postgres = null;
-      // In CI environment, PostgreSQL is provided by GitHub Actions services
-      System.out.println("CI environment detected, using external PostgreSQL service");
-    } else {
-      postgres = new PostgreSQLContainer<>(DockerImageName.parse("postgres:15-alpine"))
-        .withDatabaseName("testdb")
-        .withUsername("test")
-        .withPassword("test")
-        .withReuse(true);
-      postgres.start();
-      System.out.println("TestContainers PostgreSQL started at: " + postgres.getJdbcUrl());
-    }
+    postgres.start();
   }
 
   @DynamicPropertySource
   static void configureProperties(DynamicPropertyRegistry registry) {
-    if (CI_ENVIRONMENT) {
-      // Use GitHub Actions PostgreSQL service
-      registry.add("spring.datasource.url", () -> "jdbc:postgresql://localhost:5432/testdb");
-      registry.add("spring.datasource.username", () -> "test");
-      registry.add("spring.datasource.password", () -> "test");
-    } else {
-      // Use TestContainers
-      registry.add("spring.datasource.url", postgres::getJdbcUrl);
-      registry.add("spring.datasource.username", postgres::getUsername);
-      registry.add("spring.datasource.password", postgres::getPassword);
-    }
+    registry.add("spring.datasource.url", postgres::getJdbcUrl);
+    registry.add("spring.datasource.username", postgres::getUsername);
+    registry.add("spring.datasource.password", postgres::getPassword);
   }
 }
