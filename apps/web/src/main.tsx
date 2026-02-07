86436989d69578a86e114ee400b3bee950067ad6
JayClock <35857909+JayClock@users.noreply.github.com>
2026-02-07 17:58:37 +0800

    style(web): fix app import path casing

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-import App from './app/App';
+import App from './app/app';
 import { ResourceProvider } from '@hateoas-ts/resource-react';
 import { apiClient } from './lib/api-client';
 
