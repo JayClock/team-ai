d85e2df917f46715af37d9d9979a6c3f984097ab
JayClock <35857909+JayClock@users.noreply.github.com>
2026-02-08 10:31:01 +0800

    refactor(shells): remove unused css module from diagram component

--- libs/frontend/shells/diagram/src/lib/diagram.tsx
+++ libs/frontend/shells/diagram/src/lib/diagram.tsx
@@ -1,4 +1,3 @@
-import styles from './diagram.module.css';
 import { State } from '@hateoas-ts/resource';
 import { Diagram } from '@shared/schema';
 interface Props {
@@ -7,11 +6,7 @@ interface Props {
 
 export function ShellsDiagram(props: Props) {
   const { state } = props;
-  return (
-    <div className={styles['container']}>
-      <h1>{state.data.title}</h1>
-    </div>
-  );
+  return <h1>{state.data.title}</h1>;
 }
 
 export default ShellsDiagram;
